79f29007b57cb8df95479c2d58a1af5ff301e7ab
Vera Udodova <udodowa.vera@yandex.ru>
2023-05-24 22:49:29 +0300

    final5

--- backend/app.js
+++ backend/app.js
@@ -7,20 +7,33 @@ const { errorHandler } = require('./utils/utils');
 const NotFoundError = require('./errors/not-found-err');
 const auth = require('./middlewares/auth');
 const { signupValidator, signinValidator } = require('./validators/users');
-const { DB } = require('./config');
+const { DB, ALLOWED_CORS, DEFAULT_ALLOWED_METHODS } = require('./config');
 
 const app = express();
 
 mongoose.connect(DB);
 
+app.use((req, res, next) => {
+  const {origin} = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const {method} = req;
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 app.use('/signin', signinValidator, login);
 app.use('/signup', signupValidator, registration);
 app.use('/users', auth, require('./routes/users'));
--- backend/config.js
+++ backend/config.js
@@ -6,8 +6,17 @@ const {
 } = process.env;
 const DB = 'mongodb://127.0.0.1:27017/mestodb1';
 
+const allowedCors = [
+  'https://vera-frontend.nomoredomains.rocks',
+  'http://vera-frontend.nomoredomains.rocks',
+  'http://localhost:3000'
+];
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
 module.exports = {
   JWT_SECRET,
   PORT,
   DB,
+  ALLOWED_CORS: allowedCors,
+  DEFAULT_ALLOWED_METHODS
 };
