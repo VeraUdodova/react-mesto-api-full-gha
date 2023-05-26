f1b968eda9a4d007744c5666706a3671962b55f2
Vera Udodova <udodowa.vera@yandex.ru>
2023-05-26 08:25:25 +0300

    final

--- backend/app.js
+++ backend/app.js
@@ -4,34 +4,17 @@ const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { registration, login } = require('./controllers/users');
 const { errorHandler } = require('./utils/utils');
+const { cors } = require('./utils/cors');
 const NotFoundError = require('./errors/not-found-err');
 const auth = require('./middlewares/auth');
 const { signupValidator, signinValidator } = require('./validators/users');
-const { DB, ALLOWED_CORS, DEFAULT_ALLOWED_METHODS } = require('./config');
+const { DB } = require('./config');
 
 const app = express();
 
 mongoose.connect(DB);
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const { method } = req;
-
-  if (ALLOWED_CORS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-  return true;
-});
-
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
@@ -40,7 +23,7 @@ app.use('/signup', signupValidator, registration);
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
-app.use('/', (req, res, next) => {
+app.use('/', auth, (req, res, next) => {
   next(new NotFoundError());
 });
 app.use(errorLogger);
---
+++ backend/utils/cors.js
@@ -0,0 +1,20 @@
+const { ALLOWED_CORS, DEFAULT_ALLOWED_METHODS } = require('../config');
+
+module.exports.cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+  return true;
+};
