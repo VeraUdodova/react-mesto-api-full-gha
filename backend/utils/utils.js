50f77081483fff4bb0dd55a15a1031ca188fae10
Vera Udodova <udodowa.vera@yandex.ru>
2023-05-26 07:24:25 +0300

    final

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -21,7 +21,7 @@ const setResponse = (
 
   if (messageKey === 'message' && typeof message !== 'string') {
     result = { [messageKey]: message.join('\n') };
-  } else if (messageKey == null || messageKey === 'data') {
+  } else if (messageKey == null || messageKey === 'data' || messageKey === 'user') {
     result = message;
   } else {
     result = { [messageKey]: message };
