5042bc9d61ddc513686c1d68bac294bc0d64e0d1
Vera Udodova <udodowa.vera@yandex.ru>
2023-05-27 08:40:42 +0300

    final

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ const {
   JWT_SECRET = 'U1S52@gT5]ECPZ::F|q^hC##gl{ocRG$vh*!5F/yFAt6wHGFchBud@e.aros#SJ',
   PORT = 3000,
 } = process.env;
-const DB = 'mongodb://127.0.0.1:27017/mesto_db';
+const DB = 'mongodb://127.0.0.1:27017/mestodb';
 
 const ALLOWED_CORS = [
   'https://vera-frontend.nomoredomains.rocks',
